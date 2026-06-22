bf6c1dee676d470e0d8a05b0a0354acc567214e3
Ravi K <ravik775@gmail.com>
2026-06-22 21:12:29 +0530

    Updated Working Code

--- order-service/src/main/java/org/bgm/orderservice/controller/TestController.java
+++ order-service/src/main/java/org/bgm/orderservice/controller/TestController.java
@@ -1,13 +1,16 @@
 package org.bgm.orderservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
    @GetMapping("/")
-    String hello(){
+    String hello(@RequestParam("arg") String arg){
+
+       System.out.println("Arg: "+arg);
        return "Hello";
    }
 }
